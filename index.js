8bf11351940872b9a80d378dcab73699b70de6d2
fahmiarisma <43974794+fahmiarisma@users.noreply.github.com>
2022-12-19 10:18:59 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+const { default: WASocket, fetchLatestBaileysVersion, DisconnectReason, useMultiFileAuthState } = require('@adiwajshing/baileys')
+const Pino = require('pino')
+const { sessionName } = require('./config.json')
+const { Boom } = require('@hapi/boom')
+const { existsSync, watchFile } = require('fs')
+const path = require('path')
+let messageHandler = require('./handler/message')
+
+watchFile('./handler/message.js', () => {
+	const dir = path.resolve('./handler/message.js')
+	if (dir in require.cache) {
+		delete require.cache[dir]
+		messageHandler = require('./handler/message')
+		console.log(`reloaded message.js`)
+	}
+})
+
+const connect = async () => {
+	const { state, saveCreds } = await useMultiFileAuthState(path.resolve(`${sessionName}-session`), Pino({ level: 'silent' }))
+	let { version, isLatest } = await fetchLatestBaileysVersion()
+
+	console.log(`Using: ${version}, newer: ${isLatest}`)
+	const sock = WASocket({
+		printQRInTerminal: true,
+		auth: state,
+		logger: Pino({ level: 'silent' }),
+		version,
+	})
+	
+
+	sock.ev.on('creds.update', saveCreds)
+	sock.ev.on('connection.update', async (up) => {
+		const { lastDisconnect, connection } = up
+		if (connection) {
+			console.log('Connection Status: ', connection)
+		}
+
+		if (connection === 'close' && lastDisconnect?.error?.output?.statusCode != 401) {
+			console.log('Reconnecting...')
+			connect()
+		}
+	})
+
+	// messages.upsert
+	sock.ev.on('messages.upsert', ({ messages, type }) => {
+		if (type !== 'notify') return
+		messageHandler(sock, messages[0])
+	})
+
+	process.on('uncaughtException', (err) => {
+		console.error(err?.message)
+	})
+}
+connect()
